d1ef7f9a0d6f1b200612ce1fc33e9e33e605ece0
Shohag <shoislamNYC@gmail.com>
2023-10-27 18:59:06 -0400

    Added gitignore and begun doing css for project

--- Project2/React-app/src/App.jsx
+++ Project2/React-app/src/App.jsx
@@ -5,13 +5,21 @@ export default function App(){
   //This is a fragment, an empty tag.
   //Used because we can only return one thing, so we're returning the fragment w/ out HTML code  
   <> 
-    <Message content="Fun-Tac-Toe" />
+    <div className= 'container'>
+      <h1 className= 'title'>Fun-tac-Toe in <span>React</span></h1>
+    </div>
+   
+    <Reset/>
   </>
  )
 }
  //Only With Props, can we can pass data into a component 
- function Title(props){
-  return(<p>{props.content}</p>
-  )
+//  function Title(props){
+//   return(<h1>{props.content}</h1>
+//   )
+//  }
+ function Reset(){
+  return <button>Reset</button>
  }
+
 
